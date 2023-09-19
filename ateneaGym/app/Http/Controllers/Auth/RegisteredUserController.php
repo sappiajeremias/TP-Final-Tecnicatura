627831d097a0a0e3d9c78a19547aa9c555236971
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-09-18 21:05:05 -0300

    registro

--- ateneaGym/app/Http/Controllers/Auth/RegisteredUserController.php
+++ ateneaGym/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -14,13 +14,11 @@ use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class RegisteredUserController extends Controller
-{
+class RegisteredUserController extends Controller {
     /**
      * Display the registration view.
      */
-    public function create(): Response
-    {
+    public function create(): Response {
         return Inertia::render('Auth/Register');
     }
 
@@ -29,17 +27,22 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
-    {
+    public function store(Request $request): RedirectResponse {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:'.User::class,
+            'email' => 'required|string|email|max:255|unique:' . User::class,
+            'apellido' => 'required|string|max:255',
+            'fecha_nac' => 'required|date|max:255',
+            'dni' => 'required|integer|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'apellido' => $request->apellido,
+            'dni' => $request->dni,
+            'fecha_nac' => $request->fecha_nac,
             'password' => Hash::make($request->password),
         ]);
 
--- ateneaGym/app/Models/User.php
+++ ateneaGym/app/Models/User.php
@@ -20,6 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'apellido',
+        'dni',
+        'fecha_nac',
         'password',
     ];
 
