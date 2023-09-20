3ab860f4df00fb72fb37affb0ad879eaea00780d
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-09-20 18:38:16 -0300

    navbar

--- ateneaGym/resources/js/Layouts/nav/Nav.jsx
+++ ateneaGym/resources/js/Layouts/nav/Nav.jsx
@@ -1,3 +1,4 @@
+import NavLink from "@/Components/NavLink";
 import React, { useEffect, useState } from "react";
 
 // import { Sidebar } from 'flowbite-react';
@@ -74,7 +75,6 @@ const Nav = ({ children }) => {
                 className={` w-64 z-40 absolute bg-gray-800 shadow h-full flex flex-col justify-between transition duration-150 ease-in-out ${
                     sidebarVisible ? "" : "hidden"
                 }`}
-                
             >
                 <button
                     aria-label="Close sidebar"
@@ -102,19 +102,16 @@ const Nav = ({ children }) => {
                     </svg>
                 </button>
                 <div className="px-8">
-                    <div className="h-20 w-full flex items-center justify-center pt-8 ">
+                    <div className="h-20 w-full flex items-center justify-center pt-10 ">
                         <img
                             src="./assets/img/logo/logo_atenea.svg"
                             alt=""
                             className="bg-white h-24 rounded-lg"
                         />
                     </div>
-                    <ul className="mt-12">
-                        <li className="flex w-full justify-between text-gray-300 hover:text-gray-500 cursor-pointer items-center mb-6">
-                            <a
-                                href="#"
-                                className="flex items-center focus:outline-none focus:ring-2 focus:ring-white"
-                            >
+                    <ul className="mt-12 pt-6">
+                        <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
+                            <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white justify-between">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="icon icon-tabler icon-tabler-grid"
@@ -160,13 +157,20 @@ const Nav = ({ children }) => {
                                         rx="1"
                                     ></rect>
                                 </svg>
-                                <span className="text-sm ml-2">Dashboard</span>
-                            </a>
-                            <div className="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">
-                                5
+
+                                <NavLink
+                                    className="ps-3"
+                                    href={route("dashboard")}
+                                    active={route().current("dashboard")}
+                                >
+                                    Dashboard
+                                </NavLink>
                             </div>
+                            {/* <div className="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">
+                                5
+                            </div> */}
                         </li>
-                        <li className="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
+                        {/* <li className="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
                             <a
                                 href="#"
                                 className="flex items-center focus:outline-none focus:ring-2 focus:ring-white"
@@ -253,12 +257,9 @@ const Nav = ({ children }) => {
                                     Deliverables
                                 </span>
                             </a>
-                        </li>
-                        <li className="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
-                            <a
-                                href="#"
-                                className="flex items-center focus:outline-none focus:ring-2 focus:ring-white"
-                            >
+                        </li>*/}
+                        <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
+                            <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="icon icon-tabler icon-tabler-puzzle"
@@ -277,13 +278,53 @@ const Nav = ({ children }) => {
                                     ></path>
                                     <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1"></path>
                                 </svg>
-                                <span className="text-sm ml-2">Invoices</span>
-                            </a>
-                            <div className="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">
-                                25
+                                <NavLink
+                                    className="ps-3"
+                                    href={route("actividad.index")}
+                                    active={route().current("actividad.index")}
+                                >
+                                    Registrar Actividad
+                                </NavLink>
                             </div>
+                            {/* <div className="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">
+                                25
+                            </div> */}
                         </li>
-                        <li className="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
+                        <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
+                            <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                <svg
+                                    class=" text-gray-100 dark:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 20 18"
+                                    width="18"
+                                    height="18"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M14 3a3 3 0 1 1-1.614 5.53M15 12a4 4 0 0 1 4 4v1h-3.348M10 4.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0ZM5 11h3a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"
+                                    />
+                                </svg>
+                                <NavLink
+                                    className="ps-3"
+                                    href={route("registrarUsuarios.index")}
+                                    active={route().current(
+                                        "registrarUsuarios.index"
+                                    )}
+                                >
+                                    Registrar Usuarios
+                                </NavLink>
+                            </div>
+                            {/* <div className="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">
+                                25
+                            </div> */}
+                        </li>
+
+                        {/* <li className="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
                             <a
                                 href="#"
                                 className="flex items-center focus:outline-none focus:ring-2 focus:ring-white"
@@ -331,7 +372,7 @@ const Nav = ({ children }) => {
                                 </svg>
                                 <span className="text-sm ml-2">Settings</span>
                             </a>
-                        </li>
+                        </li>  */}
                     </ul>
                     {/* <div className="flex justify-center mt-48 mb-4 w-full">
                         <div className="relative">
@@ -371,8 +412,31 @@ const Nav = ({ children }) => {
                 </div>
 
                 <div className="flex px-8 border-t border-gray-700">
-                    <ul className="w-full flex items-center justify-between bg-gray-800">
-                        <li className="cursor-pointer text-white pt-5 pb-3">
+                    <ul className=" flex items-center justify-between  bg-gray-800">
+                        <li className="cursor-pointer text-white pb-3">
+                            <div className="flex items-center p-2 mt-12 space-x-4 justify-self-end">
+                                <img
+                                    src="https://source.unsplash.com/100x100/?portrait"
+                                    alt=""
+                                    className="w-12 h-12 rounded-lg dark:bg-gray-500"
+                                />
+                                <div>
+                                    <h2 className="text-lg font-semibold">
+                                        Leroy Jenkins
+                                    </h2>
+                                    {/* <span className="flex items-center space-x-1">
+                                        <a
+                                            rel="noopener noreferrer"
+                                            href="#"
+                                            className="text-xs hover:underline dark:text-gray-400"
+                                        >
+                                            View profile
+                                        </a>
+                                    </span> */}
+                                </div>
+                            </div>
+                        </li>
+                        <li className="cursor-pointer text-white pt-14 pb-3 ">
                             <button
                                 aria-label="show notifications"
                                 className="focus:outline-none focus:ring-2 rounded focus:ring-gray-300"
@@ -395,96 +459,6 @@ const Nav = ({ children }) => {
                                     ></path>
                                     <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"></path>
                                     <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
-                                </svg>
-                            </button>
-                        </li>
-                        <li className="cursor-pointer text-white pt-5 pb-3">
-                            <button
-                                aria-label="open chats"
-                                className="focus:outline-none focus:ring-2 rounded focus:ring-gray-300"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="icon icon-tabler icon-tabler-messages"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path
-                                        stroke="none"
-                                        d="M0 0h24v24H0z"
-                                    ></path>
-                                    <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
-                                    <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
-                                </svg>
-                            </button>
-                        </li>
-                        <li className="cursor-pointer text-white pt-5 pb-3">
-                            <button
-                                aria-label="open settings"
-                                className="focus:outline-none focus:ring-2 rounded focus:ring-gray-300"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="icon icon-tabler icon-tabler-settings"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path
-                                        stroke="none"
-                                        d="M0 0h24v24H0z"
-                                    ></path>
-                                    <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
-                                    <circle cx="12" cy="12" r="3"></circle>
-                                </svg>
-                            </button>
-                        </li>
-                        <li className="cursor-pointer text-white pt-5 pb-3">
-                            <button
-                                aria-label="open logs"
-                                className="focus:outline-none focus:ring-2 rounded focus:ring-gray-300"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="icon icon-tabler icon-tabler-archive"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path
-                                        stroke="none"
-                                        d="M0 0h24v24H0z"
-                                    ></path>
-                                    <rect
-                                        x="3"
-                                        y="4"
-                                        width="18"
-                                        height="4"
-                                        rx="2"
-                                    ></rect>
-                                    <path d="M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10"></path>
-                                    <line
-                                        x1="10"
-                                        y1="12"
-                                        x2="14"
-                                        y2="12"
-                                    ></line>
                                 </svg>
                             </button>
                         </li>
