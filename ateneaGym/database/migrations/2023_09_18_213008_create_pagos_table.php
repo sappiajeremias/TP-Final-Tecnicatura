2f059d444671db4c4ca5a6b456c6d7fff1d7c2bf
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-10-04 18:56:37 -0300

    dias disponibles para turnos

--- ateneaGym/database/migrations/2023_09_18_213008_create_pagos_table.php
+++ ateneaGym/database/migrations/2023_09_18_213008_create_pagos_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('pagos', function (Blueprint $table) {
             $table->id();
+            $table->integer('dias_disponibles');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedBigInteger('membresia_id');
@@ -27,8 +26,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('pagos');
     }
 };
