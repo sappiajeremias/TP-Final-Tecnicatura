dcfdb34dc7b39e66b5b0cf314e4c9484483d2704
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-10-06 12:42:36 -0300

    cambio de tamaño letra

--- ateneaGym/resources/js/Components/NavLink.jsx
+++ ateneaGym/resources/js/Components/NavLink.jsx
@@ -5,7 +5,7 @@ export default function NavLink({ active = false, className = '', children, ...p
         <Link
             {...props}
             className={
-                'inline-flex items-center px-1  border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
+                'inline-flex items-center px-1  border-b-2 text-md font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
                 (active
                     ? 'border-indigo-400 text-white focus:border-indigo-700 '
                     : 'border-transparent text-gray-200 focus:text-gray-700 focus:border-gray-300 ') +
