90b4b40b2b8e81cc43e088daf7b75e9ccbfaf919
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-10-06 12:42:49 -0300

    acomodo subrayado

--- ateneaGym/resources/js/Layouts/nav/Nav.jsx
+++ ateneaGym/resources/js/Layouts/nav/Nav.jsx
@@ -113,7 +113,7 @@ const Nav = ({ children, auth }) => {
                     {auth.user ? (
                         <ul className="mt-12 pt-6">
                             <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
-                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white justify-between">
+                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white justify-between gap-3">
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         className="icon icon-tabler icon-tabler-grid"
@@ -161,7 +161,7 @@ const Nav = ({ children, auth }) => {
                                     </svg>
 
                                     <NavLink
-                                        className="ps-3"
+                                        className=""
                                         href={route("dashboard")}
                                         active={route().current("dashboard")}
                                     >
@@ -171,7 +171,7 @@ const Nav = ({ children, auth }) => {
                             </li>
 
                             <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
-                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white gap-3">
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         className="icon icon-tabler icon-tabler-puzzle"
@@ -191,7 +191,6 @@ const Nav = ({ children, auth }) => {
                                         <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1"></path>
                                     </svg>
                                     <NavLink
-                                        className="ps-3"
                                         href={route("actividad.index")}
                                         active={route().current(
                                             "actividad.index"
@@ -203,7 +202,7 @@ const Nav = ({ children, auth }) => {
                             </li>
 
                             <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
-                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white gap-3">
                                     <svg
                                         className=" text-gray-100 dark:text-white"
                                         aria-hidden="true"
@@ -222,7 +221,6 @@ const Nav = ({ children, auth }) => {
                                         />
                                     </svg>
                                     <NavLink
-                                        className="ps-3"
                                         href={route("usuarios.index")}
                                         active={route().current(
                                             "registrarUsuarios.index"
@@ -236,7 +234,7 @@ const Nav = ({ children, auth }) => {
                             </div> */}
                             </li>
                             <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
-                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white gap-3">
                                     <svg
                                         className=" text-gray-100 dark:text-white"
                                         aria-hidden="true"
@@ -255,7 +253,6 @@ const Nav = ({ children, auth }) => {
                                         />
                                     </svg>
                                     <NavLink
-                                        className="ps-3"
                                         href={route("especialidad.index")}
                                         active={route().current(
                                             "especialidad.index"
@@ -269,7 +266,7 @@ const Nav = ({ children, auth }) => {
                             </div> */}
                             </li>
                             <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
-                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white gap-3">
                                     <svg
                                         className=" text-gray-100 dark:text-white"
                                         aria-hidden="true"
@@ -288,22 +285,16 @@ const Nav = ({ children, auth }) => {
                                         />
                                     </svg>
                                     <NavLink
-                                        className="ps-3"
                                         href={route("turnos.index")}
-                                        active={route().current(
-                                            "turnos.index"
-                                        )}
+                                        active={route().current("turnos.index")}
                                     >
                                         Sacar Turno
                                     </NavLink>
                                 </div>
-                                {/* <div className="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">
-                                25
-                            </div> */}
                             </li>
 
                             <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
-                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white gap-3">
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         width="16"
@@ -322,7 +313,6 @@ const Nav = ({ children, auth }) => {
                                         />
                                     </svg>
                                     <NavLink
-                                        className="ps-3"
                                         href={route("logout")}
                                         method="post"
                                         as="button"
@@ -331,66 +321,13 @@ const Nav = ({ children, auth }) => {
                                         Cerrar Sesion
                                     </NavLink>
                                 </div>
-                                {/* <div className="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">
-                                25
-                            </div> */}
                             </li>
-
-                            {/* <li className="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
-                            <a
-                                href="#"
-                                className="flex items-center focus:outline-none focus:ring-2 focus:ring-white"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="icon icon-tabler icon-tabler-stack"
-                                    width="18"
-                                    height="18"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <polyline points="12 4 4 8 12 12 20 8 12 4" />
-                                    <polyline points="4 12 12 16 20 12" />
-                                    <polyline points="4 16 12 20 20 16" />
-                                </svg>
-                                <span className="text-sm ml-2">Inventory</span>
-                            </a>
-                        </li>
-                        <li className="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center">
-                            <a
-                                href="#"
-                                className="flex items-center focus:outline-none focus:ring-2 focus:ring-white"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="icon icon-tabler icon-tabler-settings"
-                                    width="18"
-                                    height="18"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                    <circle cx="12" cy="12" r="3" />
-                                </svg>
-                                <span className="text-sm ml-2">Settings</span>
-                            </a>
-                        </li>  */}
                         </ul>
                     ) : (
                         <>
                             <ul className="mt-12 pt-6">
                                 <li className="flex w-full justify-between text-gray-100 cursor-pointer items-center mb-6">
-                                    <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white justify-between">
+                                    <div className="flex items-center focus:outline-none focus:ring-2 focus:ring-white justify-between gap-3">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             width="16"
@@ -406,7 +343,6 @@ const Nav = ({ children, auth }) => {
                                             />
                                         </svg>
                                         <NavLink
-                                            className="ps-3"
                                             href={route("login")}
                                             active={route().current("login")}
                                         >
