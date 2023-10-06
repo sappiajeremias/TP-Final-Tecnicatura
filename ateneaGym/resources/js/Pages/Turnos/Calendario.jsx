aa53f616805877513a80301891c17908ba3bc4a9
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-10-06 12:43:05 -0300

    calendario

---
+++ ateneaGym/resources/js/Pages/Turnos/Calendario.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+
+import React from "react";
+
+const Calendario = ({ turnos }) => {
+    const [fecha, setFecha] = useState(new Date());
+    function abrirTurnos(params) {}
+    return (
+        <div className="container w-50">
+            <Calendar
+                onChange={setFecha}
+                value={fecha}
+                onClickDay={(date, event) => console.log(date)}
+                tileContent={({ activeStartDate, date, view }) => {
+                    if (
+                        view === "month" &&
+                        turnos.includes(date.toISOString().slice(0, 10))
+                    ) {
+                        return (
+                            <div className="bg-red-400 h-2 w-2 m-auto border rounded-lg mt-1"></div>
+                        );
+                    } else {
+                        return null;
+                    }
+                }}
+            />
+
+            {/* {fecha} */}
+        </div>
+    );
+};
+
+export default Calendario;
