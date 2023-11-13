709893ce4a00e1827f02de83c55aa76ca34067f5
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-11-13 10:17:56 -0300

    acomodo pagos

--- ateneaGym/app/Http/Controllers/PagoController.php
+++ ateneaGym/app/Http/Controllers/PagoController.php
@@ -11,8 +11,10 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use MercadoPago;
+use MercadoPago\Client\MerchantOrder\MerchantOrderClient;
 use MercadoPago\Client\Preference\PreferenceClient;
 use MercadoPago\MercadoPagoConfig;
+use MercadoPago\Net\MPSearchRequest;
 use MercadoPago\Resources\Payment as ResourcesPayment;
 use MercadoPago\Resources\Payment;
 use MercadoPago\Resources\Payment\Payer;
@@ -74,92 +76,76 @@ class PagoController extends Controller {
     public function destroy(Pago $pago) {
     }
     public function crearPreference(Request $request) {
-        // Crear un objeto de preferencia
-        $preference = new Preference();
+        // dd($request);
 
-        // Crear un ítem en la preferencia
-        $item = new Item();
-        $item->id = 'item-ID-1234';
-        $item->title = 'Mi producto';
-        $item->currency_id = 'BRL';
-        $item->picture_url = 'https://www.mercadopago.com/org-img/MP3/home/logomp3.gif';
-        $item->description = 'Descripción del artículo';
-        $item->category_id = 'art';
-        $item->quantity = 1;
-        $item->unit_price = 75.76;
-        $preference->items = array($item);
+        // $fecha = Carbon::now();
+        // $fecha->addDays(30);
+        // $membresia = Membresia::where('id', $request->membresia_id)->first();
 
-        // Información del pagador
-        $payer = new PreferencePayer();
-        $payer->name = 'João';
-        $payer->surname = 'Silva';
-        $payer->email = 'user@email.com';
-        $payer->phone = array(
-            'area_code' => '11',
-            'number' => '4444-4444'
-        );
-        $payer->identification = array(
-            'type' => 'CPF',
-            'number' => '19119119100'
-        );
-        $payer->address = array(
-            'street_name' => 'Street',
-            'street_number' => 123,
-            'zip_code' => '06233200'
-        );
-        $preference->payer = $payer;
+        // $pago = Pago::create([
+        //     'user_id' => Auth()->user()->id,
+        //     'membresia_id' => $request->membresia_id,
+        //     'medio_pago' => $medio,
+        //     'dias_disponibles' => ($membresia->dias_disponibles) * 4,
+        //     'fecha_vencimiento' => $fecha,
+        // ]);
 
-        // URLs de redirección
-        $back_urls = array(
-            'success' =>  route("procesar.respuesta"),
-            'failure' =>  route("procesar.respuesta"),
-            'pending' =>  route("procesar.respuesta")
-        );
-        $preference->back_urls = $back_urls;
+        // $preferencia = $request->id;
 
-        // Configuraciones de pago
-        $payment_methods = array(
-            'excluded_payment_methods' => [],
-            'excluded_payment_types' => [
-                array(
-                    'id' => "ticket"
-                )
-            ],
-            'installments' => 1
-        );
-        $preference->payment_methods = $payment_methods;
-
-        // $preference->notification_url = 'https://www.your-site.com/ipn';
-        $preference->statement_descriptor = 'MEUNEGOCIO';
-        $preference->external_reference = 'Reference_1234';
-        $preference->expires = true;
-        $preference->expiration_date_from = '2016-02-01T12:00:00.000-04:00';
-        $preference->expiration_date_to = '2016-02-28T12:00:00.000-04:00';
-        $preferencia = new PreferenceClient($preference);
-        // $preference->save();
-        dd($preference);
-        return redirect()->back()->with('preferenceId', $preferencia->id);
+        // return;
     }
 
 
-    public function estadoPago(Request $request) {  #requestUri: "/procesar-respuesta-pago?collection_id=1319284021&collection_status=approved&payment_id=1319284021&status=approved&external_reference=null&payment_type=account_money&merchant_order_id=12946912919&preference_id=1530492018-53f07270-d110-46e7-a46c-eb541e69636e&site_id=MLA&processing_mode=aggregator&merchant_account_id=null ◀"
+    public function estadoPago(Request $request) {
         $_GET['status'];
 
         $payment_info = $_GET;
-        dd($payment_info);
+
+        // dd($payment_info);
         switch ($payment_info['status']) {
             case 'approved':
-                $payment = new Payment();
+                return Inertia::render('Dashboard', [
+                    'mensaje' => 'El pago se registro con exito',
+                ]);
+                // $verif = false;
+                // $fecha = Carbon::now();
+                // $fecha->addDays(30);
 
-                $payment->external_reference = $payment_info['external_reference'];
-                $payment->status = $payment_info['status'];
-                $payment->site_id = $payment_info['site_id'];
-                $payment->processing_mode = $payment_info['processing_mode'];
-                dd($payment);
+
+
+                // $client = new PreferenceClient();
+
+                // // $search_request = new MPSearchRequest(1, 0, [
+                // //     "sponsor_id" => "0",
+                // //     "external_reference" => "null",
+                // //     "site_id" => "MLA",
+                // //     "marketplace" => "NONE"
+                // // ]);
+                // // $client->search($search_request);
+                // // dd($client);
+                // dd($client->get(intval($payment_info['preference_id'])));
+                // $membresia = Membresia::where('id', $request->membresia_id)->first();
+
+                // $pago = Pago::create([
+                //     'user_id' => $request->user_id,
+                //     'membresia_id' => $request->membresia_id,
+                //     'medio_pago' => $request->payment_type,
+                //     'dias_disponibles' => ($membresia->dias_disponibles) * 4,
+                //     'fecha_vencimiento' => $fecha,
+                // ]);
+                // $pago->save();
+                // return redirect()->route('dashboard');
+                // $payment = new Payment();
+
+                // $payment->external_reference = $payment_info['external_reference'];
+                // $payment->status = $payment_info['status'];
+                // $payment->site_id = $payment_info['site_id'];
+                // $payment->processing_mode = $payment_info['processing_mode'];
+                // dd($payment);
                 break;
 
             default:
-                # code...
+                dd('hola');
                 break;
         }
     }
