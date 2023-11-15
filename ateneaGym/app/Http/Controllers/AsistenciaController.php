3c37389ea93a328a1d6380c80cafc9cc11f839d4
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-11-15 17:22:16 -0300

    modelo y controlador asistencia

--- ateneaGym/app/Http/Controllers/AsistenciaController.php
+++ ateneaGym/app/Http/Controllers/AsistenciaController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AsistenciaController extends Controller
-{
-    //
+class AsistenciaController extends Controller {
+    public function index() {
+    }
+    public function store(Request $request) {
+    }
+    public function update(Request $request, RutinaAlumno $rutinaAlumno) {
+    }
+    public function destroy(RutinaAlumno $rutinaAlumno) {
+    }
 }
--- ateneaGym/app/Models/Asistencia.php
+++ ateneaGym/app/Models/Asistencia.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Asistencia extends Model
-{
+class Asistencia extends Model {
     use HasFactory;
+    protected $fillable = [
+        'alumno_id',
+        'especialidad_id',
+        'fecha'
+    ];
+
+    public function alumno() {
+        return $this->belongsTo(Alumno::class);
+    }
+    public function especialidad() {
+        return $this->belongsTo(Especialidad::class);
+    }
 }
