a371b2a25c0f0cb846858983e9d6cd070e15f327
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-11-21 10:06:30 -0300

    mensaje error si no se puede renovar

--- ateneaGym/resources/js/Pages/Membresia/CardMembresia.jsx
+++ ateneaGym/resources/js/Pages/Membresia/CardMembresia.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { router, useForm } from "@inertiajs/react";
+import { router, useForm, usePage } from "@inertiajs/react";
 import Swal from "sweetalert2";
 
 function CardMembresia({ membresia, auth }) {
@@ -9,10 +9,11 @@ function CardMembresia({ membresia, auth }) {
             membresia_id: id,
         });
 
-     // Estado para almacenar el resultado SOAP
+    // Estado para almacenar el resultado SOAP
 
     const clickHandler = (e) => {
         e.preventDefault();
+
         Swal.fire({
             title: "Estás seguro?",
             text: "Reserva de membresía " + descripcion,
@@ -21,12 +22,20 @@ function CardMembresia({ membresia, auth }) {
             confirmButtonText: "Confirmar",
         }).then((result) => {
             if (result.isConfirmed) {
-                post(`/realizarPago`,membresia.id);
+                post(`/realizarPago`, {
+                    onError: (response) => {
+                        console.log(response);
+                        Swal.fire({
+                            icon: "error",
+                            text: response.message,
+                        });
+                    },
+                });
             }
         });
     };
 
-  const [numberToWords, setNumberToWords] = useState("");
+    const [numberToWords, setNumberToWords] = useState("");
 
     useEffect(() => {
         // Realiza la solicitud SOAP al cargar el componente
@@ -69,7 +78,7 @@ function CardMembresia({ membresia, auth }) {
 
             const parser = new DOMParser();
             const xmlDoc = parser.parseFromString(responseBody, "text/xml");
-      console.log(xmlDoc);
+            console.log(xmlDoc);
             // Encuentra el elemento <m:NumberToWordsResult>
             const numberToWordsResult = xmlDoc.querySelector(
                 "NumberToWordsResult"
@@ -116,63 +125,63 @@ function CardMembresia({ membresia, auth }) {
                     </p>
                 )}
                 <ul role="list" className="space-y-5 my-7">
-                      <li className="flex space-x-3 items-center">
-            <svg
-              className="flex-shrink-0 w-4 h-4  text-pink-500 dark:text-pink-600"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              {membresia.dias_disponibles} dias disponibles por
-              semana
-            </span>
-          </li>
-          <li className="flex space-x-3 items-center">
-            <svg
-              className="flex-shrink-0 w-4 h-4  text-pink-500 dark:text-pink-600"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              Rutinas personalizadas
-            </span>
-          </li>
-          <li className="flex space-x-3 items-center">
-            <svg
-              className="flex-shrink-0 w-4 h-4 text-pink-500 dark:text-pink-600"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              Profesor
-            </span>
-          </li>
-          <li className="flex space-x-3 items-center">
-            <svg
-              className="flex-shrink-0 w-4 h-4 text-pink-500 dark:text-pink-600"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              Clases o Musculación
-            </span>
-          </li>
+                    <li className="flex space-x-3 items-center">
+                        <svg
+                            className="flex-shrink-0 w-4 h-4  text-pink-500 dark:text-pink-600"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            {membresia.dias_disponibles} dias disponibles por
+                            semana
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <svg
+                            className="flex-shrink-0 w-4 h-4  text-pink-500 dark:text-pink-600"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            Rutinas personalizadas
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <svg
+                            className="flex-shrink-0 w-4 h-4 text-pink-500 dark:text-pink-600"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            Profesor
+                        </span>
+                    </li>
+                    <li className="flex space-x-3 items-center">
+                        <svg
+                            className="flex-shrink-0 w-4 h-4 text-pink-500 dark:text-pink-600"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+                            Clases o Musculación
+                        </span>
+                    </li>
                 </ul>
             </div>
             <div className="px-6 pb-2 flex justify-end">
