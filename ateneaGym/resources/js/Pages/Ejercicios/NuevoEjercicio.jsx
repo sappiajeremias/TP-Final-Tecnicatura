e7d1826b6a50cd076fb1b3bc53b4af2e0af56769
Agustina Rossi <maria.rossi@est.fi.uncoma.edu.ar>
2023-11-21 10:06:46 -0300

    carga de imagen vista

--- ateneaGym/resources/js/Pages/Ejercicios/NuevoEjercicio.jsx
+++ ateneaGym/resources/js/Pages/Ejercicios/NuevoEjercicio.jsx
@@ -14,10 +14,31 @@ const NuevoEjercicio = (props) => {
         musculo: "",
         imagen: "",
     });
+    console.log(data.imagen);
     const cerrar = props.cerrarModal;
-    const listaParteCuerpo = ['Brazos', 'Pecho', 'Hombros', 'Espalda', 'Piernas','Abdominales'];
-    const listaMusculos = ['Biceps', 'Triceps', 'Hombros', 'Trapecios', 'Dorsales','Pectorales', 'Abdominales', 'Gluteos','Cuadriceps','Aductores','Isquiotibiales', 'Gemélos'];
-   //console.log(props.cerrarModal);
+    const listaParteCuerpo = [
+        "Brazos",
+        "Pecho",
+        "Hombros",
+        "Espalda",
+        "Piernas",
+        "Abdominales",
+    ];
+    const listaMusculos = [
+        "Biceps",
+        "Triceps",
+        "Hombros",
+        "Trapecios",
+        "Dorsales",
+        "Pectorales",
+        "Abdominales",
+        "Gluteos",
+        "Cuadriceps",
+        "Aductores",
+        "Isquiotibiales",
+        "Gemélos",
+    ];
+    //console.log(props.cerrarModal);
     const submit = (e) => {
         e.preventDefault();
         post(`/ejercicio`, {
@@ -87,7 +108,10 @@ const NuevoEjercicio = (props) => {
                     <InputError message={errors.musculo} className="mt-2" />
                 </div>
                 <div className="mt-4">
-                    <InputLabel htmlFor="parte_cuerpo" value="Parte del cuerpo" />
+                    <InputLabel
+                        htmlFor="parte_cuerpo"
+                        value="Parte del cuerpo"
+                    />
 
                     <select
                         name="parte_cuerpo"
@@ -95,29 +119,48 @@ const NuevoEjercicio = (props) => {
                         id="parte_cuerpo"
                         className="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm "
                         value={data.parte_cuerpo}
-                        onChange={(e) => setData("parte_cuerpo", e.target.value)}
+                        onChange={(e) =>
+                            setData("parte_cuerpo", e.target.value)
+                        }
                     >
-                        <option value={1} >Seleccione la parte del cuerpo</option>
+                        <option value={1}>
+                            Seleccione la parte del cuerpo
+                        </option>
                         {listaParteCuerpo.map((musc, index) => (
                             <option key={index} value={musc}>
                                 {musc}
                             </option>
                         ))}
                     </select>
-                    <InputError message={errors.parte_cuerpo} className="mt-2" />
+                    <InputError
+                        message={errors.parte_cuerpo}
+                        className="mt-2"
+                    />
                 </div>
                 <div className="mt-2">
-                    <InputLabel htmlFor="imagen" value="Imagen" />
+                    <InputLabel
+                        htmlFor="imagen"
+                        value="Ingrese URL o seleccione una imagen"
+                    />
 
                     <TextInput
                         id="imagen"
                         type="text"
                         name="imagen"
-                        value={data.imagen}
+                        value={
+                            data.imagen.name ? data.imagen.name : data.imagen
+                        }
                         className="mt-1 block w-full"
                         autoComplete="imagen"
                         isFocused={true}
                         onChange={(e) => setData("imagen", e.target.value)}
+                    />
+                    <input
+                        id="imagen"
+                        type="file"
+                        accept="image/*"
+                        className="mt-3"
+                        onChange={(e) => setData("imagen", e.target.files[0])}
                     />
 
                     <InputError message={errors.imagen} className="mt-2" />
@@ -126,7 +169,7 @@ const NuevoEjercicio = (props) => {
                     <button
                         onClick={props.cerrarModal}
                         type="button"
-                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ml-4"
+                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ml-4 mt-2"
                     >
                         Cancelar
                     </button>
