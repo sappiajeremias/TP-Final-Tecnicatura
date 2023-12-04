8e68237b6765a6639860e89507e8f298fdc51eca
Jeremias <82519584+sappiajeremias@users.noreply.github.com>
2023-12-04 08:58:51 -0300

    Update RutinaSeeder

--- ateneaGym/database/seeders/RutinaSeeder.php
+++ ateneaGym/database/seeders/RutinaSeeder.php
@@ -12,10 +12,10 @@ class RutinaSeeder extends Seeder {
      */
     public function run(): void {
         //
-        $rutina1 = Rutina::create(['nombre'=>'probando1','mes' => 'noviembre','dificultad'=>'Facil','profesor_id' => 1, 'dia_semana' => 'lunes']);
-        $rutina2 = Rutina::create(['nombre'=>'probando2','mes' => 'noviembre','dificultad'=>'Medio', 'profesor_id' => 1, 'dia_semana' => 'miercoles']);
-        $rutina3 = Rutina::create(['nombre'=>'probando3','mes' => 'noviembre','dificultad'=>'Medio', 'profesor_id' => 1, 'dia_semana' => 'viernes']);
-        $rutina4 = Rutina::create(['nombre'=>'probando4','mes' => 'noviembre','dificultad'=>'Dificil', 'profesor_id' => 2, 'dia_semana' => 'martes']);
-        $rutina5 = Rutina::create(['nombre'=>'probando5','mes' => 'noviembre','dificultad'=>'Dificil', 'profesor_id' => 2, 'dia_semana' => 'jueves']);
+        $rutina1 = Rutina::create(['nombre'=>'Piernas','mes' => 'noviembre','dificultad'=>'Facil','profesor_id' => 1, 'dia_semana' => 'lunes']);
+        $rutina2 = Rutina::create(['nombre'=>'Empuje','mes' => 'noviembre','dificultad'=>'Medio', 'profesor_id' => 1, 'dia_semana' => 'miercoles']);
+        $rutina3 = Rutina::create(['nombre'=>'Fullbody','mes' => 'noviembre','dificultad'=>'Medio', 'profesor_id' => 1, 'dia_semana' => 'viernes']);
+        $rutina4 = Rutina::create(['nombre'=>'Cardio','mes' => 'noviembre','dificultad'=>'Dificil', 'profesor_id' => 2, 'dia_semana' => 'martes']);
+        $rutina5 = Rutina::create(['nombre'=>'Espalda y Bíceps','mes' => 'noviembre','dificultad'=>'Dificil', 'profesor_id' => 2, 'dia_semana' => 'jueves']);
     }
 }
